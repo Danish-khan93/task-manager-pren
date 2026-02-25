70792f329a47d680f7db5b74cc01c6b3a64433f3
Danish khan <danishshoukat1993@gmail.com>
2026-02-25 06:24:33 +0500

    make custom component files and start from button (work in process)

--- task-manager-pern/frontend/src/App.tsx
+++ task-manager-pern/frontend/src/App.tsx
@@ -1,8 +1,20 @@
+import { CustomButton } from "./component";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <CustomButton label="testing button" type="button" variant="text" />
+      <CustomButton icon={"plus"} type="button" variant="icon" />
+      <CustomButton
+        icon={"plus"}
+        label={"Add"}
+        type="button"
+        variant="iconText"
+      />
+      <CustomButton icon={"edit"} type="button" variant="icon" />
+      <CustomButton icon={"delete"} type="button" variant="icon" />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ task-manager-pern/frontend/src/component/CustomButton.tsx
@@ -0,0 +1,37 @@
+import type { FC } from "react";
+import { FaPlus } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+
+const iconMap = {
+  plus: <FaPlus />,
+  edit: <MdEdit />,
+  delete: <MdDelete />,
+};
+type Props = {
+  type: "button" | "submit";
+  variant: "icon" | "text" | "iconText";
+  label?: string;
+  icon?: keyof typeof iconMap; // allows only iconMap key names (auto-suggest & type-safe)
+};
+
+const CustomButton: FC<Props> = (props) => {
+  // learning tips
+  // in destructuring assing value  with = if use : mean rename the name
+  const { type = "button", label, icon, variant = "iconText" } = props;
+
+  const styling = {
+    icon: "w-[40px] h-[40px] p-2 rounded-full bg-white border-4 border-red-300 flex justify-center items-center",
+    text: "max-w-42 py-1 px-2 rounded-lg bg-red-300 whitespace-nowrap",
+    iconText: "py-1 px-2 rounded-lg flex justify-center items-center gap-3 bg-red-300 whitespace-nowrap",
+  };
+
+  return (
+    <button type={type} className={`${variant && styling[variant]} m-1`}>
+      <span>{icon && iconMap[icon]}</span>
+      <span>{label && label}</span>
+    </button>
+  );
+};
+
+export default CustomButton;

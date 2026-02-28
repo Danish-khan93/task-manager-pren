5de9671239902d39594cf60e353d865183b01792
Danish khan <danishshoukat1993@gmail.com>
2026-02-28 17:15:33 +0500

    make some changes

--- task-manager-pern/frontend/src/feature/dashboard/pages/Dashboard.tsx
+++ task-manager-pern/frontend/src/feature/dashboard/pages/Dashboard.tsx
@@ -3,7 +3,7 @@ import MainLayout from "../../../layout/mainLayout/MainLayout";
 const Dashboard = () => {
   return (
     <MainLayout>
-      <div>Dashboard</div>
+      <div>Dashboard </div>
     </MainLayout>
   );
 };
